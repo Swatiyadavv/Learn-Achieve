79c37527926b9a7333caf32d61b38ec781f7d75b
Swatiyadavv <swatiyadav66145@gmail.com>
2025-06-09 16:45:01 +0530

    cors

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const path = require('path');         
-
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const adminRoutes = require("./routes/adminRoutes");
 const mockTestRoutes = require('./routes/mockTestRoutes')
 const packageRoutes = require('./routes/packageRoutes');
+const cors = require('cors')
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
  const mongoURI = process.env.MONGO_URI || "your_mongodb_atlas_connection_string_here";
 mongoose
@@ -23,13 +24,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
-
-
-
-
-
-

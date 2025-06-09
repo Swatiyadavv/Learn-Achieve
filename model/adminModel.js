be3328c6d0b7e60033f58beb6034e44b4d4bd0c1
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-06-09 11:01:01 +0530

    name added

--- model/adminModel.js
+++ model/adminModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const ADMIN_ROLE = require("../enums/adminRoleEnum");
 
 const adminSchema = new mongoose.Schema({
+  name:{type:String},
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, enum: Object.values(ADMIN_ROLE), default: ADMIN_ROLE.ADMIN },

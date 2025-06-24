35a60b62832eb440b6442a30a82e13f0668fffda
Swatiyadavv <swatiyadav66145@gmail.com>
2025-06-24 15:47:45 +0530

    added

--- routes/packageRoutes.js
+++ routes/packageRoutes.js
@@ -3,26 +3,19 @@ const router = express.Router();
 const packageController = require('../controller/packageController');
 const upload = require('../middleware/uploadMiddleware');
 const { verifyToken } = require('../middleware/authMiddleware'); //  Token middleware import
-
 //  Add package (protected + image upload)
 router.post('/add', verifyToken, upload.single('image'), packageController.addPackage);
-
 //  Delete package by ID (protected)
 router.delete('/delete/:id', verifyToken, packageController.deletePackage);
-
 // Get all packages (protected)
 router.get('/get', verifyToken, packageController.getAllPackages);
 // Update package
 router.put('/update/:id', verifyToken, upload.single('image'), packageController.updatePackage);
-
-
 router.get('/search', verifyToken, packageController.searchPackages);
 router.delete('/delete-multiple', verifyToken, packageController.deleteMultiplePackages);
 router.get('/paginated', packageController.getPaginatedPackages);
 //get packages with paginated
 router.get('/paginated', verifyToken, packageController.getPaginatedPackages);
-
-
-// users
+// users 
 router.get('/packages', packageController.getAllPackages);
-module.exports = router;
+module.exports = router;   

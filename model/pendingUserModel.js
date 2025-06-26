b2aaa078e5ecb22e0ed86a74242ce3618ff9352b
Swatiyadavv <swatiyadav66145@gmail.com>
2025-06-26 18:03:27 +0530

    added

--- model/pendingUserModel.js
+++ model/pendingUserModel.js
@@ -1,3 +1,15 @@
+// const mongoose = require("mongoose");
+
+// const pendingUserSchema = new mongoose.Schema({
+//   email: String,
+//   password: String,
+//   otp: String,
+//   otpExpire: Date,
+// });
+
+// module.exports = mongoose.model("PendingUser", pendingUserSchema);
+
+
 const mongoose = require("mongoose");
 
 const pendingUserSchema = new mongoose.Schema({
@@ -5,6 +17,7 @@ const pendingUserSchema = new mongoose.Schema({
   password: String,
   otp: String,
   otpExpire: Date,
+  referredBy: { type: String, default: null } // ✅ Add this
 });
 
 module.exports = mongoose.model("PendingUser", pendingUserSchema);

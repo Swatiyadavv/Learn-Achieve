e1d928cf9040ce66729b7763ee5937f90ef25e0d
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-07-01 10:07:06 +0530

    done classMaster

--- routes/classMasterRoutes.js
+++ routes/classMasterRoutes.js
@@ -2,26 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { protect } = require("../middleware/authMiddleware");
 const classMasterController = require('../controller/classMasterController');
-// Create class
-router.post('/', protect, classMasterController.createClass);
+
+
 // Get all classes (active + inactive) with pagination + search
 router.get('/all', protect, classMasterController.getAllClasses);
-// Get all active classes with pagination + search
-router.get('/active', protect, classMasterController.getPaginatedActiveClasses);
-// Get single class by ID
-router.get('/:id', protect, classMasterController.getClassById);
-// Update class by ID
-router.put('/:id', protect, classMasterController.updateClass);
-
 
 // add and update with same routes
-router.post('/', protect, classMasterController.addOrUpdateClass);
-
-// Get all classes (active + inactive) with pagination + search
-router.get('/all', protect, classMasterController.getAllClasses);
+router.post('/addandupdate', protect, classMasterController.addOrUpdateClass);
 
 // DELETE single and multiple classes by IDs
 router.delete('/', protect, classMasterController.deleteClass);
+
 // Toggle active/inactive
 router.put('/toggle/:id', protect, classMasterController.toggleActive);
 module.exports = router;

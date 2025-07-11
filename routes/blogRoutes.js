63082b21da5c92f4596065162f9ea327251081d9
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-07-11 17:51:44 +0530

    done

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -2,17 +2,18 @@ const express = require("express");
 const router = express.Router();
 const blogController = require("../controller/blogController");
 const upload = require("../middleware/uploadMiddleware");
+const { protect } = require("../middleware/authMiddleware");
 
 router.post("/add", upload.fields([
   { name: "featuredImage", maxCount: 1 },
   { name: "mainImage", maxCount: 1 }
-]), blogController.addOrUpdateBlog);
+]), protect,blogController.addOrUpdateBlog);
 
-router.get("/", blogController.getAllBlogs);
+router.get("/", protect,blogController.getAllBlogs);
 
-router.patch("/status/:id", blogController.toggleBlogStatus);
+router.patch("/status/:id",protect, blogController.toggleBlogStatus);
 
-router.delete("/delete", blogController.deleteBlog); 
+router.delete("/delete", protect,blogController.deleteBlog); 
 
 
 module.exports = router;

aec9e2757782ab4bc9541480a20c649c214a7254
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-07-23 12:20:09 +0530

    done

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,33 +1,4 @@
-// const studentService = require("../service/studentService");
 
-// exports.registerStudent = async (req, res) => {
-//   try {
-//     const result = await studentService.registerStudent(req.body);
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
-
-// exports.verifyStudentOTP = async (req, res) => {
-//   const { email, otp } = req.body;
-//   try {
-//     const result = await studentService.verifyStudentOTP(email, otp);
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
-
-// exports.loginStudent = async (req, res) => {
-//   const { email, password } = req.body;
-//   try {
-//     const result = await studentService.loginStudent(email, password);
-//     res.status(200).json(result);
-//   } catch (err) {
-//     res.status(400).json({ message: err.message });
-//   }
-// };
 const studentService = require("../service/studentService");
 
 exports.registerStudent = async (req, res) => {

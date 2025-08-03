53335833db5ca4885958f72bc22d2ce954d78614
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-08-03 16:25:20 +0530

    generatecode

--- utils/generateCode.js
+++ utils/generateCode.js
@@ -1,18 +1,3 @@
-// const Coordinator = require('../model/coordinatorModel');
-
-// const generateUniqueCode = async () => {
-//   let code;
-//   let exists = true;
-
-//   while (exists) {
-//     code = Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit
-//     exists = await Coordinator.exists({ uniqueCode: code });
-//   }
-
-//   return code;
-// };
-
-// module.exports = generateUniqueCode;
 const Coordinator = require('../model/coordinatorModel');
 
 const generateUniqueCode = async () => {

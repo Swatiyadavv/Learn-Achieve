5a860631bb291476b5f953df345c13c84b3ad776
Vaishnavi Gandhi <vaishnavii8319@gmail.com>
2025-08-06 18:37:03 +0530

    done

--- service/reviewerService.js
+++ service/reviewerService.js
@@ -43,15 +43,44 @@
 // module.exports = { getQuestions,getAllReviewHistory };
  const Question = require('../model/questionModel');
 
-const getQuestions = async ({ classId, subjectId, medium }) => {
-  const query = {};
-  if (classId) query.classId = classId;
-  if (subjectId) query.subjectId = subjectId;
-  if (medium) query.medium = medium;
+// const Question = require('../model/questionModel');
+const SubQuestion = require('../model/subQuestionModel'); // import this
 
-  return await Question.find(query)
-    .populate('classId', 'name')
-    .populate('subjectId', 'name');
+const getQuestions = async (filters) => {
+  const query = {};
+  if (filters.classId) query.classId = filters.classId;
+  if (filters.subjectId) query.subjectId = filters.subjectId;
+  if (filters.medium) query.medium = filters.medium;
+
+  const questions = await Question.find(query)
+    .populate('classId', '_id className')
+    .populate('subjectId', '_id subjectName');
+
+  const result = [];
+
+  for (const q of questions) {
+    let formattedQuestion = { ...q._doc };
+
+    // If Poem/Paragraph, fetch subquestions
+    if (q.typeOfQuestion === 'Poem' || q.typeOfQuestion === 'Paragraph') {
+      const subQs = await SubQuestion.find({ parentId: q._id });
+
+      formattedQuestion.subQuestions = subQs.map((subQ) => ({
+        questionText: subQ.questionText,
+        options: subQ.options,
+        correctAnswer: subQ.correctAnswer
+      }));
+
+      formattedQuestion.options = []; // so frontend doesn’t expect main options
+    } else {
+      // Make sure options array always exists
+      formattedQuestion.options = q.options?.length ? q.options : [];
+    }
+
+    result.push(formattedQuestion);
+  }
+
+  return result;
 };
 
 const getAllReviewHistory = async (filters = {}) => {
